126f307694644e3777169112b41ea3c06fb81ea2
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2025-07-30 14:53:47 +0530

    Update NetworkService.kt

--- app/src/main/java/me/amitshekhar/mvvm/data/api/NetworkService.kt
+++ app/src/main/java/me/amitshekhar/mvvm/data/api/NetworkService.kt
@@ -5,9 +5,7 @@ import me.amitshekhar.mvvm.utils.AppConstant.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
-import javax.inject.Singleton
 
-@Singleton
 interface NetworkService {
 
     @Headers("X-Api-Key: $API_KEY", "User-Agent: ABC")
